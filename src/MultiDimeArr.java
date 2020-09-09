5a39de699f6a5f5e75202a20ad52a0830e9fd628
Makhi7 <mvmangxola22@gmail.com>
2020-09-09 16:04:52 +0200

    five

---
+++ src/MultiDimeArr.java
@@ -0,0 +1,23 @@
+public class MultiDimeArr {
+
+    public static void main(String[] args) {
+
+        int[][] multiArr = {{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}, {2, 4, 6, 8, 10, 12, 14, 16, 18, 20}};
+
+        for (int x = 0; x < multiArr.length; x++) {
+
+       /*     int mThree = 0;
+            int mFive  = 0;
+            multiArr[0][2] = mThree;
+            multiArr[0][5] = mThree;
+            multiArr[0][8] = mThree;
+
+            multiArr[1][4] = mFive;
+            multiArr[1][9] = mFive;
+            System.out.println("The array " + multiArr[x]);
+                                                                */
+
+        }
+    }
+
+}
